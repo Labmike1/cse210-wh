9edcf9bdc84e7887ff9c4057853fd11dd26dfa2d
Labmike1 <bay22002@byui.edu>
2023-01-11 19:58:02 -0700

    Submiting activities

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // generate a random number // 
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+        
+        // Console.Write("What is your grade percentage? ");
+        // Console.WriteLine($"Your grade is: {number}");
+
+        int guess =0;
+
+         // We could also use a do-while loop here...
+        while (guess != number)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (number > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (number < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,43 @@
 using System;
+using System.Collections.Generic;
 
 class Program
-{
+{ 
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        int number = -1;
+        int sumar = 0; 
+        float average = 0;
+        while (number != 0)
+        {
+            Console.Write("  Enter a list of numbers, type 0 when finished.");
+             
+            string userlist = Console.ReadLine();
+            number = int.Parse(userlist);
+
+            
+            if (number != 0){
+                numbers.Add(number);
+
+                sumar = sumar + number;
+            }
+            average = ((float)sumar) / numbers.Count;
+             
+             
+        }
+        int max = numbers[0]; 
+             foreach (int Number in numbers)
+        {
+            if (Number > max)
+            {
+                // if this number is greater than the max, we have found the new max!
+                max = Number;
+            }
+        }   
+    Console.WriteLine($"The sum is: {sumar}");
+    Console.WriteLine($"The average is: {average}");     
+    Console.WriteLine($"The max is: {max}");     
     }
 }
